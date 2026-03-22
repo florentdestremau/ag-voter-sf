d5cc76a0347e3b5aa9a16dae44efc3771d93ce52
Florent <florent.destremau@gmail.com>
2026-03-22 22:11:53 +0100

    Fix choice prototype HTML, replace vote buttons with radio+submit

--- assets/controllers/choices_controller.js
+++ assets/controllers/choices_controller.js
@@ -9,10 +9,7 @@ export default class extends Controller {
     const prototypeEl = this.element.querySelector('[data-choices-prototype]')
     const container = this.element.querySelector('[data-choices-container]')
     const html = prototypeEl.dataset.choicesPrototype.replace(/__name__/g, this.index++)
-    const row = document.createElement('div')
-    row.className = 'choice-row card mb-2'
-    row.innerHTML = `<div class="card-body py-2 px-3"><div class="d-flex gap-2 align-items-center"><div class="flex-grow-1">${html}</div><button type="button" class="btn btn-sm btn-outline-danger" data-action="click->choices#removeChoice"><i class="bi bi-trash"></i></button></div></div>`
-    container.appendChild(row)
+    container.insertAdjacentHTML('beforeend', html)
   }
 
   removeChoice (event) {
--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,9 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 import PollController from './controllers/poll_controller.js';
 import ChoicesController from './controllers/choices_controller.js';
-import VoteFormController from './controllers/vote_form_controller.js';
 
 const app = startStimulusApp();
 app.register('poll', PollController);
 app.register('choices', ChoicesController);
-app.register('vote-form', VoteFormController);
